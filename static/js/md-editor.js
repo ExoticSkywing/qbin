9053b7bd0b010d478ccfdf46217478671022cdbd
exoticskywing <akeemkeebler495@gmail.com>
2025-11-07 11:03:28 +0000

    name init project 及帮助文档位置优化

--- static/js/md-editor.js
+++ static/js/md-editor.js
@@ -28,7 +28,7 @@ class QBinMDEditor extends QBinEditorBase {
                     },
                     'search',
                 ];
-            config.toolbarRight = ['mySettings', 'togglePreview', 'undo', 'redo', ];
+            config.toolbarRight = ['mySettings', 'myGuide', 'togglePreview', 'undo', 'redo', ];
             config.sidebar = null;
             config.bubble = false;
         }else {
@@ -58,7 +58,7 @@ class QBinMDEditor extends QBinEditorBase {
                     'redo',
                     'export',
                 ];
-            config.toolbarRight = ['mySettings', 'togglePreview', 'shortcutKey', 'wordCount'];
+            config.toolbarRight = ['mySettings', 'togglePreview', 'shortcutKey', 'myGuide', 'wordCount'];
             config.bubble = ['bold', 'italic', 'underline', 'strikethrough', 'sub', 'sup', 'quote', '|', 'size', 'color'];
             config.sidebar = null;
         }
@@ -77,6 +77,16 @@ class QBinMDEditor extends QBinEditorBase {
             this.togglePasswordPanel(true); // 传入 true 表示是点击触发
           }
         });
+        const customGuide = Cherry.createMenuHook('帮助文档', {
+          icon: {
+            type: 'svg',
+            content: '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path stroke-dasharray="64" stroke-dashoffset="64" d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="64;0"/></path><path stroke-dasharray="16" stroke-dashoffset="16" d="M9 10c0 -1.66 1.34 -3 3 -3c1.66 0 3 1.34 3 3c0 0.98 -0.47 1.85 -1.2 2.4c-0.73 0.55 -1.3 0.6 -1.8 1.6"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.2s" values="16;0"/></path><path stroke-dasharray="2" stroke-dashoffset="2" d="M12 17v0.01"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.8s" dur="0.2s" values="2;0"/></path></g></svg>',
+            iconStyle: 'width: 18px; height: 18px; vertical-align: middle;',
+          },
+          onClick: () => {
+            window.open('/r/guide', '_blank', 'noopener,noreferrer');
+          }
+        });
         const toolbars = {
             showToolbar: true,
             toolbar: [],
@@ -85,6 +95,7 @@ class QBinMDEditor extends QBinEditorBase {
             sidebar: [],
             customMenu: {
                 mySettings: customSettings,
+                myGuide: customGuide,
             },
             toc: true,
             float: false,
